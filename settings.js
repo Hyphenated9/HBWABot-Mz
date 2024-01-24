a628b9b688b2dba9dc537942aa348615ae1b39de
Hyphenated9 <152174228+Hyphenated9@users.noreply.github.com>
2024-01-24 11:05:45 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,8 +6,8 @@ global.AntiCallBot = true // false a i dah chuan, rawn call-tu che automatic a b
 global.GroupWcMsg = true // false a i dah chuan welcome message i off ang
 global.autoTyping = false // true a i dah chuan i type la a lang ang
 global.autoRecord = false // true a i dah chuan Voice record angin i lang ang
-global.available =true // false a i dah chuan offline angin a awm ang
-global.autoread =   false// true a i dah chuan message zawng zawng read vek zelin a awm ang 
+global.available = true // false a i dah chuan offline angin a awm ang
+global.autoread =false// true a i dah chuan message zawng zawng read vek zelin a awm ang 
 global.autoread_status = false // true a i dah chuan auto status view in a awm ang
 
 //info
